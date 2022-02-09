849269cec8da71f4cc9dd43d0afc7c943800e4a1
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-02-09 17:03:15 +0100

    Add possibility to simulate just the detector in air

--- src/geom/geometry.py
+++ src/geom/geometry.py
@@ -5,6 +5,7 @@ from pyg4ometry import visualisation as vis
 import numpy as np
 
 enable_cavities = False
+enable_pyramid = True
 
 print("INFO: building geometry")
 
@@ -40,7 +41,7 @@ concrete_s = geant4.solid.Union("Concrete", pyramid_s, ground_s,
 
 # rock_mat = geant4.MaterialPredefined('G4_CONCRETE')
 element = geant4.nist_element_2geant4Element
-rock_mat = geant4.MaterialCompound("Limestone", 10, 10, registry=reg)
+rock_mat = geant4.MaterialCompound("Limestone", 2.2, 10, registry=reg)
 rock_mat.add_element_massfraction(element('G4_H'),  0.01)
 rock_mat.add_element_massfraction(element('G4_C'),  0.001)
 rock_mat.add_element_massfraction(element('G4_O'),  0.529107)
@@ -53,29 +54,38 @@ rock_mat.add_element_massfraction(element('G4_Ca'), 0.044)
 rock_mat.add_element_massfraction(element('G4_Fe'), 0.014)
 
 concrete_l = geant4.LogicalVolume(concrete_s, rock_mat, 'Concrete', reg)
-geant4.PhysicalVolume([0, 0, 0], [0, 0, -world_height/2 + h + ground_depth - eps, 'm'],
-        concrete_l, 'Concrete', world_l, reg)
+if enable_pyramid:
+    geant4.PhysicalVolume([0, 0, 0], [0, 0, -world_height/2 + h + ground_depth - eps, 'm'],
+            concrete_l, 'Concrete', world_l, reg)
 
+cavities_pos = [0, 19.5, -75, 'm']
 if enable_cavities:
     # read CAD model of cavities in
     r = stl.Reader("khufu-pyramid.stl", solidname="Cavities", scale=1000, registry=reg)
     cavities_s = r.getSolid()
     cavities_l = geant4.LogicalVolume(cavities_s, "G4_AIR", "Cavities", reg)
-    geant4.PhysicalVolume([0, 0, 0], [0, 19.5, -75, 'm'],
+    geant4.PhysicalVolume([0, 0, 0], cavities_pos,
             cavities_l, 'Cavities', concrete_l, reg)
 
 det_s = geant4.solid.Box('Detector', 3, 3, 0.05, reg, 'm')
 det_l = geant4.LogicalVolume(det_s, 'G4_PHOTO_EMULSION', 'Detector', reg)
 
-queen_cham_floor = [0, -22, 18, 'm']
+if enable_cavities:
+    queen_cham_floor = [0, -22, 18, 'm']
+    mother = cavities_l
+elif enable_pyramid:
+    queen_cham_floor = [0, -2.5, -57, 'm']
+    mother = concrete_l
+else:
+    queen_cham_floor = [0, -2.5, -47.5, 'm']
+    mother = world_l
+
 geant4.PhysicalVolume([0, 0, 0], queen_cham_floor, det_l,
-        'LowerPanel', cavities_l if enable_cavities else concrete_l,
-        reg, copyNumber=0)
+        'LowerPanel', mother, reg, copyNumber=0)
 
 queen_cham_floor[2] += 1
 geant4.PhysicalVolume([0, 0, 0], queen_cham_floor, det_l,
-        'UpperPanel', cavities_l if enable_cavities else concrete_l,
-        reg, copyNumber=1)
+        'UpperPanel', mother, reg, copyNumber=1)
 
 # cavities_l.checkOverlaps()
 
@@ -84,9 +94,9 @@ w = gdml.Writer()
 w.addDetector(reg)
 w.write('geometry.gdml')
 
-# print("INFO: starting VTK GUI")
-# v = vis.VtkViewer()
-# v.addLogicalVolume(reg.getWorldVolume())
-# v.addAxes(100000)
-# v.setCameraFocusPosition()
-# v.view()
+print("INFO: starting VTK GUI")
+v = vis.VtkViewer()
+v.addLogicalVolume(reg.getWorldVolume())
+v.addAxes(100000)
+v.setCameraFocusPosition()
+v.view()
