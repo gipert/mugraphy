e2d62b4802de3a5a10aa42cdb1bb110987ea1aa5
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-02-11 21:32:56 +0100

    Add script to make macros

--- hpc/make.py
+++ hpc/make.py
@@ -1,1 +1,16 @@
+import sys
 import os
+
+batch = sys.argv[1]
+
+os.makedirs(batch + '/mac')
+os.makedirs(batch + '/out')
+os.makedirs(batch + '/log')
+
+with open(batch + '.mac') as f:
+    text = f.read()
+
+for i in range(0, 200):
+    mtext = text.replace('$OUTPUT', f'out/detector-hits-{i}.root')
+    with open(f'{batch}/mac/{batch}-{i}.mac', 'w') as fout:
+        fout.write(mtext)
