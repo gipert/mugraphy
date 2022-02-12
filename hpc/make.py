516051e5f4c68b3a0e3b1642f3df5053b30d2095
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-02-12 17:33:15 +0100

    Improve scripts for job submission

--- hpc/make.py
+++ hpc/make.py
@@ -1,16 +1,17 @@
 import sys
 import os
 
-batch = sys.argv[1]
+template_macro = sys.argv[1]
+batch = sys.argv[2]
 
-os.makedirs(batch + '/mac')
-os.makedirs(batch + '/out')
-os.makedirs(batch + '/log')
+os.makedirs(f'runs/{batch}/mac')
+os.makedirs(f'runs/{batch}/out')
+os.makedirs(f'runs/{batch}/log')
 
-with open(batch + '.mac') as f:
+with open(template_macro) as f:
     text = f.read()
 
-for i in range(0, 200):
-    mtext = text.replace('$OUTPUT', f'out/detector-hits-{i}.root')
-    with open(f'{batch}/mac/{batch}-{i}.mac', 'w') as fout:
+for i in range(1, 201):
+    mtext = text.replace('$OUTPUT', f'runs/out/detector-hits-{i}.root')
+    with open(f'runs/{batch}/mac/{batch}-{i}.mac', 'w') as fout:
         fout.write(mtext)
