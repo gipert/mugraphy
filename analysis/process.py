c612385fcb6c81d4d8a4a850081c95cadca6b12f
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-02-27 13:45:26 +0100

    Simplify analysis script

--- analysis/process.py
+++ analysis/process.py
@@ -1,55 +1,51 @@
+import sys
 import uproot
-import awkward as ak
-# import numpy as np
+# import awkward as ak
+import numpy as np
 # import hist
-import matplotlib
+# import matplotlib
 import matplotlib.pyplot as plt
-import mplhep as hep
+# import mplhep as hep
 from hist import Hist
 
 # hep.style.use(["fira", "firamath"])
 
-with uproot.open('detector-hits.root:ntuples') as events:
+with uproot.open(sys.argv[1] + ':ntuples') as events:
     df = events.arrays(library='ak')
+
+# df = uproot.concatenate("hstat/*.root:ntuples")
 
 print('INFO: ', len(df), ' total events')
 
-# select only rows with two-panel hits
-sel = ak.num(df.panel, axis=1) == 2
-df = df[sel]
+theta = df.theta[..., 0]
+phi = df.phi[..., 0]
 
-print('INFO: ', len(df), ' events with two-panel hit')
-
-# track direction - azimuthal angles
-Dx = df.xhit[..., 0] - df.xhit[..., 1]
-Dy = df.yhit[..., 0] - df.yhit[..., 1]
-Dz = ak.mean(df.zhit[..., 0] - df.zhit[..., 1])
-
-ang_x = Dx / Dz
-ang_y = Dy / Dz
+ang_x = np.tan(theta) * np.cos(phi)
+ang_y = np.tan(theta) * np.sin(phi)
 
 h1 = (
     Hist.new
-    .Reg(100, -2, 2, name='x', label=r'$\tan\theta_x$')
-    .Reg(100, -2, 2, name='y', label=r'$\tan\theta_y$')
+    .Reg(100, -1, 1, name='x', label=r'$\tan\theta_x$')
+    .Reg(100, -1, 1, name='y', label=r'$\tan\theta_y$')
     .Double()
 )
 h1.fill(x=ang_x, y=ang_y)
 
+plt.figure(figsize=(9, 8))
 # h1.plot()
 # h1.project("x").plot()
 # h1.project("y").plot()
-plt.figure(figsize=(8, 8))
+# plt.figure(figsize=(8, 8))
 h1.plot2d_full(top_ls='-', side_yerr=False, top_yerr=False)
+# h1.plot2d_full(top_ls='-', side_yerr=False, top_yerr=False, main_cmap='rainbow')
 
-theta = df.theta[..., 0]
-h2 = Hist.new.Reg(100, 0, 180, name='x', label=r'Azimuthal angle $\theta$ (deg)').Double()
+h2 = Hist.new.Reg(100, 0, np.pi, name='x', label=r'Azimuthal angle $\theta$ (deg)').Double()
 h2.fill(theta)
 # h2.plot(yerr=False)
 
 # deposited energy
 he = Hist.new.Reg(100, 0, 100, name='x', label='Energy (GeV)').Double()
-he.fill(ak.sum(df.energy, axis=-1))
-# he.plot(ls='-', yerr=False)
+he.fill(df.energy[..., 0])
+# he.plot(yerr=False)
 
 plt.show()
